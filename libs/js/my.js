52ac20c204ecdabd68025a5214710da489004244
runningsparrow <runningsparrow@gmail.com>
2018-03-05 15:31:58 +0800

    bootstrapless0301

---
+++ libs/js/my.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Administrator on 2018/3/1.
+ */
+/**
+ * for bs1301
+ */
+$("a[rel=tootip]").tooltip();
+$("a[rel=popover]").popover();
+
+
+/**
+ * for bs1501
+ * 修改图片切换时间间隔为2s
+ **/
+
+$('#carousel1').carousel({
+    interval: 2000
+});
